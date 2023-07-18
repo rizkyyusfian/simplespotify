47f0f4d2a01d2b345d9faf8155f7f249893979a5
Muhammad Rizky Yusfian Yusuf <rizky.yusfian16@gmail.com>
2023-07-18 19:46:10 +0900

    server & db
    make express server and setup mongodb connection

--- index.js
+++ index.js
@@ -1,54 +1,36 @@
-// import Song from './models/playlist.js ';
-import express from 'express';
-// const express = require('express');
+// dotenv.config();
+
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import bodyParser from 'body-parser';
+// import router from './routes/routes.js';
+
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = require('./routes/routes.js');
+const cors = require('cors');
+
+
+
+// Connect to the database
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const databse = mongoose.connection;
+
+databse.on('error', (error) => console.log(error));
+databse.once('connected', () => console.log('Connected to database'));
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-
-// In-memory storage for the playlist
-let playlist = [];
-
-// Playlist model
-class Song {
-  constructor(title, artists, url) {
-    this.title = title;
-    this.artists = artists;
-    this.url = url;
-    this.playCount = 0;
-  }
-}
-
-// Add a song to the playlist
-app.post('/playlist', (req, res) => {
-  const { title, artists, url } = req.body; //get the data from the body
-  const song = new Song(title, artists, url); // Create a new song object
-  playlist.push(song);  //Add the song to the playlist
-  res.status(201).json({ message: 'Song added to the playlist' }); // Send a response
-});
-
-// Play a song from the playlist
-app.post('/playlist/play', (req, res) => {
-  const { title } = req.body; // Get the title from the body
-  const song = playlist.find((s) => s.title === title); // Find the song in the playlist by title
-  if (!song) {
-    res.status(404).json({ error: 'Song not found in the playlist' }); // Send an error response
-  } else {
-    song.playCount++; // Increment the play count
-    res.status(200).json({ message: `Now playing: ${song.title}` }); // Send a response
-  }
-});
-
-// Get the list of songs in the playlist
-app.get('/playlist', (req, res) => {
-  const { sort } = req.query; // Get the sort query parameter
-  let sortedPlaylist = []; // Create a new array to store the sorted playlist
-  if (sort === 'most-played') {
-    sortedPlaylist = [...playlist].sort((a, b) => b.playCount - a.playCount); // Sort the playlist by play count
-  } else {
-    sortedPlaylist = [...playlist]; // Copy the playlist
-  }
-  res.status(200).json(sortedPlaylist); // Send the sorted playlist as a response
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', router);
 
 // Start the server
 app.listen(3000, () => {
